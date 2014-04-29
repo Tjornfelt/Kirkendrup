ce9689216551e6a5deb7af79514aad4cd9a3a3fa
Mads Tjørnfelt <mads@lait.dk>
2014-04-29 20:05:31 +0200

    Basic stuff to get things started

---
+++ Kirkendrup/Kirkendrup/Controllers/FrontPageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kirkendrup.Models;
+using Umbraco.Web.Models;
+
+namespace Kirkendrup.Controllers
+{
+    public class FrontPageController : MasterController
+    {
+        //
+        // GET: /FrontPage/
+
+        public ActionResult FrontPage(RenderModel renderModel)
+        {
+			var model = new FrontPageModel();
+
+            return View(model);
+        }
+    }
+}
---
+++ Kirkendrup/Kirkendrup/Controllers/MasterController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Kirkendrup.Controllers
+{
+    public class MasterController : RenderMvcController
+    {
+        //
+        // GET: /Master/
+
+        protected override ViewResult View(string viewName, string masterName, object model)
+        {
+
+			//Move all "Global content" into the viewbag - it shouldnt be accessible in the model
+			/*
+			var global = new GlobalModel();
+			global.isFrontpage = CurrentPage.Id == rootNode.Id;
+			global.GoogleAnalytics = GoogleAnalyticsMapper.Map<GoogleAnalyticsModel>(CurrentPage, Umbraco);
+			global.Breadcrumb = NavigationItemMapper.Map(new List<NavigationItemModel>(), CurrentPage.AncestorsOrSelf(),
+				CurrentPage, Umbraco);
+			global.TopMenu = NavigationItemMapper.Map(new List<NavigationItemModel>(), CurrentPage.AncestorsOrSelf(1),
+				CurrentPage, Umbraco);
+			global.InShopContext = ShopHelper.InShopContext(CurrentPage);
+			global.Seo = SeoMapper.Map<SeoModel>(CurrentPage, global.InShopContext, Umbraco);
+			SetLanguageDropdown(global, rootNode);
+			global.SocialMetaData = GetSocialMetaData(global, CurrentPage, rootNode);
+			ViewBag.Global = global;
+			*/
+
+			return base.View(viewName, masterName, model);
+        }
+    }
+}
